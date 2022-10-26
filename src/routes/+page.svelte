e06db046136f327bed7f2306fa9ad4462a44c6fe
Thanh Ngoc 1 <phanthanhngoc11@gmail.com>
2022-10-26 15:05:40 +0700

    fix some bug

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
-<script>
+<!-- <script>
   import { Button } from 'flowbite-svelte';
 </script>
 
 <h1>Welcome to SvelteKit + TailwindCSS + Flowbite Components </h1>
 
-<Button color="red">We use Flowbite from now !</Button>
+<Button color="red">We use Flowbite from now !</Button> -->
 
 
 <script>
