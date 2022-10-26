2c4600b96778f137b8a7f11c2ba46828da84ec62
Thanh Ngoc 1 <phanthanhngoc11@gmail.com>
2022-10-26 15:23:52 +0700

    change look

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,7 @@
 <script>
   import { Card, Button } from "flowbite-svelte";
 </script>
+<section class="h-screen flex flex-col items-center justify-center">
 <Card>
   <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Welcome to SvelteKit + TailwindCSS + Flowbite Components</h5>
   <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
@@ -19,3 +20,4 @@
     Read more <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 ml-2"><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" /></svg>
   </Button>
 </Card>
+</section>
